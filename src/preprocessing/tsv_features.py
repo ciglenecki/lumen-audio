18e7c75e4310203c37366ee2d9e1bd5f5e0e1373
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2023-04-08 03:33:36 +0200

    add preprocessing for embedding plotting

---
+++ src/preprocessing/tsv_features.py
@@ -0,0 +1,76 @@
+import json
+from pathlib import Path
+
+import pandas as pd
+from tqdm import tqdm
+
+import src.config.config_defaults as config_defaults
+
+OUT_DIR = embeddings_dir = config_defaults.PATH_IRMAS_TRAIN_FEATURES
+
+embeddings = None
+labels = []
+sample_paths = []
+name = []
+instrument_names = []
+
+
+def get_fields(item):
+    embedding = item["embedding"]
+    sample_path = item["sample_path"]
+    label = item["label"]
+    instrument = item["instrument"]
+    instrument_name = item["instrument_name"]
+    return embedding, sample_path, label, instrument, instrument_name
+
+
+for i, json_path in tqdm(enumerate(embeddings_dir.glob("*.json"))):
+    item = json.load(open(json_path))
+    (
+        embedding,
+        sample_path,
+        label,
+        instrument,
+        instrument_name,
+    ) = get_fields(item)
+
+    # Create embeddings dict if it isn't created yet
+    if embeddings is None:
+        embeddings = {f"e{i}": [] for i in range(len(embedding))}
+
+    # Save each embedding
+    for e_idx, e in enumerate(embedding):
+        embeddings[f"e{e_idx}"].append(e)
+
+    instrument_names.append(instrument_name)
+    sample_paths.append(sample_path)
+
+dict = {
+    **embeddings,
+}
+
+dict_metadata = {
+    "instrument": instrument_names,
+    "path": sample_paths,
+}
+
+df = pd.DataFrame.from_dict(dict)
+df_metadata = pd.DataFrame.from_dict(dict_metadata)
+
+df_path = Path(OUT_DIR, "ast_embeddings.tsv")
+df_meta_path = Path(OUT_DIR, "ast_embeddings_meta.tsv")
+
+df.to_csv(
+    df_path,
+    sep="\t",
+    header=False,
+    index=False,
+)
+print("Saved file:", str(df_path))
+
+df_metadata.to_csv(
+    df_meta_path,
+    sep="\t",
+    index=False,
+)
+print("Saved file:", str(df_meta_path))
