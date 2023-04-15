0f735574a9dfa090a77aff3269431a30c9da5f01
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2023-04-15 03:25:44 +0200

    remove comment

--- src/model/model.py
+++ src/model/model.py
@@ -28,7 +28,8 @@ def get_data_input_type(model_enum: SupportedModels) -> ModelInputDataType:
 
 
 def get_model(
-    config: ConfigDefault, loss_function: torch.nn.modules.loss  # maknut pl_args argument, ako ce nekome trebat nek doda
+    config: ConfigDefault,
+    loss_function: torch.nn.modules.loss,
 ) -> tuple[pl.LightningModule, ModelInputDataType]:
     from src.model.model_torch import TORCHVISION_CONSTRUCTOR_DICT, TorchvisionModel
 
